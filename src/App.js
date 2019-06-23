f88e5e503e94435e857058772ccf6e688d09674a
Quang Viet <quangnvph05487@fpt.edu.vn>
2019-06-24 00:51:34 +0700

    preparing mockup

--- src/App.js
+++ src/App.js
@@ -1,70 +1,39 @@
-import React from "react";
+import React from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-function App() {
+function Index() {
+    return <h2>Home</h2>
+}
+function About() {
+    return <h2>About</h2>
+}
+function User() {
+    return <h2>User</h2>
+}
+function AppRouter() {
     return (
         <Router>
             <div>
-                <Header />
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to={"/"}>Home</Link>
+                        </li>
+                        <li>
+                            <Link to={"/about"}>About</Link>
+                        </li>
+                        <li>
+                            <Link to={"/user"}>User</Link>
+                        </li>
+                    </ul>
+                </nav>
 
-                <Route exact path="/" component={Home} />
-                <Route path="/about" component={About} />
-                <Route path="/topics" component={Topics} />
+                <Route path={"/"} exact component={Index}/>
+                <Route path={"/about"} component={About}/>
+                <Route path={"/user"} component={User}/>
             </div>
         </Router>
-    );
+    )
 }
 
-function Home() {
-    return <h2>Home</h2>;
-}
-
-function About() {
-    return <h2>About</h2>;
-}
-
-function Topic({ match }) {
-    return <h3>Requested Param: {match.params.id}</h3>;
-}
-
-function Topics({ match }) {
-    return (
-        <div>
-            <h2>Topics</h2>
-
-            <ul>
-                <li>
-                    <Link to={`${match.url}/components`}>Components</Link>
-                </li>
-                <li>
-                    <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-                </li>
-            </ul>
-
-            <Route path={`${match.path}/:id`} component={Topic} />
-            <Route
-                exact
-                path={match.path}
-                render={() => <h3>Please select a topic.</h3>}
-            />
-        </div>
-    );
-}
-
-function Header() {
-    return (
-        <ul>
-            <li>
-                <Link to="/">Home</Link>
-            </li>
-            <li>
-                <Link to="/about">About</Link>
-            </li>
-            <li>
-                <Link to="/topics">Topics</Link>
-            </li>
-        </ul>
-    );
-}
-
-export default App;
+export default AppRouter;
