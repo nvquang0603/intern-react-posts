3f23af84dcd1d6846c5fed0dbc94e6e7795dad22
nvquang0603 <quangnvph05487@fpt.edu.vn>
2019-06-26 23:54:09 +0700

    QUANG Filter on click submit button

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -39,47 +39,46 @@ class Edit extends Component {
     render() {
         return (
             <div>
-                <div className="jumbotron">
-                    <h2 className="text-center">Edit Topic</h2>
-                    <div className="col-12">
-                        <div className="input-group mb-3">
-                            <div className="input-group-prepend">
-                                <span className="input-group-text">Title</span>
+                <div className={"jumbotron"}>
+                    <h2 className={"text-center"}>Edit Topic</h2>
+                    <div className={"col-12"}>
+                        <div className={"input-group mb-3"}>
+                            <div className={"input-group-prepend"}>
+                                <span className={"input-group-text"}><i className={"fas fa-comment-alt"}/></span>
                             </div>
                             <input
-                                type="text"
-                                className="form-control"
-                                id="title"
+                                type={"text"}
+                                className={"form-control"}
+                                id={"title"}
                                 name={"title"}
                                 onChange={this.handleChange}
                                 value={this.state.title}
                             />
                         </div>
                     </div>
-                    <div className="col-12">
-                        <div className="input-group mb-3">
-                            <div className="input-group-prepend">
-                                <span className="input-group-text">Content</span>
+                    <div className={"col-12"}>
+                        <div className={"input-group mb-3"}>
+                            <div className={"input-group-prepend"}>
+                                <span className={"input-group-text"}><i className={"fas fa-file-contract"} style={{fontSize: '20px'}}/></span>
                             </div>
                             <textarea
-                                rows="10"
-                                className="form-control"
-                                id="content"
+                                rows={"10"}
+                                className={"form-control"}
                                 name={"content"}
                                 onChange={this.handleChange}
                                 value={this.state.content}
                             />
                         </div>
                     </div>
-                    <div className="col-12">
-                        <div className="input-group mb-3">
-                            <div className="input-group-prepend">
-                                <span className="input-group-text">Author</span>
+                    <div className={"col-12"}>
+                        <div className={"input-group mb-3"}>
+                            <div className={"input-group-prepend"}>
+                                <span className={"input-group-text"}><i className={"fas fa-at"}/></span>
                             </div>
                             <input
-                                type="text"
-                                className="form-control"
-                                id="author"
+                                type={"text"}
+                                className={"form-control"}
+                                id={"author"}
                                 name={"author"}
                                 onChange={this.handleChange}
                                 value={this.state.author}
@@ -87,11 +86,11 @@ class Edit extends Component {
                         </div>
                     </div>
 
-                    <div className="form-check form-check-inline">
-                        <label className={"form-check-label"}>Active </label>
+                    <div className={"form-check form-check-inline p-3"}>
+                        <label className={"form-check-label"}>Active status &nbsp;</label>
                         <input
                             className={"form-check-input"}
-                            type="checkbox"
+                            type={"checkbox"}
                             name={"active"}
                             onChange={this.handleChange}
                             value={this.state.active}
@@ -103,19 +102,19 @@ class Edit extends Component {
 
                     <div className="button-group text-center">
                         <button
-                            type="submit"
-                            className="btn btn-success"
+                            type={"submit"}
+                            className={"btn btn-success mr-2"}
                             onClick={this.handleSubmit.bind(this)}
                         >
-                            <i className="far fa-paper-plane" style={{fontSize: '18px'}}/>
+                            <i className={"far fa-paper-plane"} style={{fontSize: '18px'}}/>&nbsp;
                             Submit
                         </button>
                         <button
-                            type="submit"
-                            className="btn btn-danger"
+                            type={"submit"}
+                            className={"btn btn-danger"}
                             onClick={this.handleReset}
                         >
-                            <i className="fas fa-eraser" style={{fontSize: '18px'}}/>
+                            <i className={"fas fa-eraser"} style={{fontSize: '18px'}}/>&nbsp;
                             Reset
                         </button>
                     </div>
