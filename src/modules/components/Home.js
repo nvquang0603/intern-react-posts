2a810f7e91f5571296d555b060590f9410b01d46
GiangLN <junnycp3@gmail.com>
2019-07-14 23:04:01 +0700

    Nam Giang commit 14/07

--- src/modules/components/Home.js
+++ src/modules/components/Home.js
@@ -10,21 +10,23 @@ class Home extends Component {
     render() {
 
         return (
-            this.props.posts.map(post => {
-                return (
-                    <div className="container">
-                        <div className="row">
-                            <div key={post.id} className="card" style={{width: '18rem'}}>
-                                <div className="card-body">
-                                    <h5 className="card-title">{post.title}</h5>
-                                    <h6 className="card-subtitle mb-2 text-muted">{post.active === true ? 'Active' : 'Inactive'}</h6>
-                                    <p className="card-text">{post.content}</p>
+            <div className={"container"}>
+                <div className={"row"}>
+                    {this.props.posts.map(post => {
+                        return (
+                            <div className={"col-md-4 my-2"}>
+                                <div key={post.id} className="card w-100 h-100">
+                                    <div className="card-body">
+                                        <h5 className="card-title">#{post.id}: {post.title}</h5>
+                                        <h6 className="card-subtitle mb-2 text-muted">{post.active===true?'Active':'Inactive'}</h6>
+                                        <p className="card-text">{post.content}</p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>
-                )
-            })
+                        )
+                    })}
+                </div>
+            </div>
         )
 
     }
